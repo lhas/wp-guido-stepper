2b880974eb230974d32102d45a288cb9f9462fd6
lhas <luizhrqas@gmail.com>
2017-04-02 14:03:21 -0300

    styled thank you slide

--- public/js/wp-guido-stepper-public.js
+++ public/js/wp-guido-stepper-public.js
@@ -2,21 +2,32 @@
 	'use strict';
 
 	$(document).ready(function() {
-	 $('.guido-stepper').slick({
-		 infinite: false,
-		 arrows: false,
-		 swipe: false,
-	 });
+		$('.guido-stepper').slick({
+			infinite: false,
+			arrows: false,
+			swipe: false,
+		});
 
-	 $('.guido-stepper-slide-item').on('click', function() {
-		$('.guido-stepper').slick('slickNext');
-		return false;
-	 });
+		var values = [];
 
-	 $('.guido-stepper-form').on('submit', function() {
-		$('.guido-stepper').slick('slickNext');
-		return false;
-	 });
+		$('.guido-stepper-slide-item').on('click', function() {
+			var headline = $(this).data('headline');
+			var value = $(this).data('value');
+
+			values.push({
+				headline: headline,
+				value: value,
+			});
+
+			$('.guido-stepper').slick('slickNext');
+			return false;
+		});
+
+		$('.guido-stepper-form').on('submit', function() {
+			console.log(values);
+			$('.guido-stepper').slick('slickNext');
+			return false;
+		});
 	});
 
 })( jQuery );
